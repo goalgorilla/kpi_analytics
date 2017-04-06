de5382df2e76595d7e9c49f5e0af4fb8dd25d741
Taras Kruts <taras.kruts@lemberg.co.uk>
2017-04-06 19:37:48 +0300

    ECI-220 by ribel: Remove morris JS library from module

--- assets/js/morris-chart.js
+++ assets/js/morris-chart.js
@@ -1,20 +1,20 @@
 (function ($, Drupal) {
 
-    'use strict';
+  'use strict';
 
-    Drupal.behaviors.kpiAnalyticsRenderMorris = {
-        attach: function (context, settings) {
-            $(context).find('div.morris_chart').once('renderChart').each(function () {
-                var uuid = $(this).attr('id');
-                var options = settings.kpi_analytics.morris.chart[uuid].options;
+  Drupal.behaviors.kpiAnalyticsRenderMorris = {
+    attach: function (context, settings) {
+      $(context).find('div.morris_chart').once('renderChart').each(function () {
+        var uuid = $(this).attr('id');
+        var options = settings.kpi_analytics.morris.chart[uuid].options;
 
-                if (!options.plugin) {
-                  options.plugin = 'Line';
-                }
-
-                new Morris[options.plugin](options);
-            });
+        if (!options.plugin) {
+          options.plugin = 'Line';
         }
-    };
+
+        new Morris[options.plugin](options);
+      });
+    }
+  };
 
 })(jQuery, Drupal);
