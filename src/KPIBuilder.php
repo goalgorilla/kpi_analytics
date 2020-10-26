8da5c718ca7ed885d832585b1e111f5cfce65750
Dmytrii Kaiun <dmytrii.kaiun@lemberg.co.ua>
2020-10-26 18:43:02 +0200

    Issue #3177657 by tBKoT: Set config form to use it in Layout builder

--- src/KPIBuilder.php
+++ src/KPIBuilder.php
@@ -2,10 +2,21 @@
 
 namespace Drupal\kpi_analytics;
 
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Entity\Entity\EntityViewDisplay;
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\Plugin\Context\Context;
+use Drupal\Core\Plugin\Context\ContextDefinition;
+use Drupal\Core\Plugin\Context\EntityContext;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\kpi_analytics\Plugin\KPIDataFormatterManager;
 use Drupal\kpi_analytics\Plugin\KPIDatasourceManager;
 use Drupal\kpi_analytics\Plugin\KPIVisualizationManager;
+use Drupal\layout_builder\Entity\LayoutEntityDisplayInterface;
+use Drupal\layout_builder\SectionStorage\SectionStorageManagerInterface;
+use Drupal\layout_builder\SectionStorageInterface;
 
 /**
  * Class KPIBuilder.
@@ -43,6 +54,20 @@ class KPIBuilder implements KPIBuilderInterface {
   protected $kpiVisualizationManager;
 
   /**
+   * The section storage manager.
+   *
+   * @var \Drupal\layout_builder\SectionStorage\SectionStorageManagerInterface
+   */
+  protected $sectionStorageManager;
+
+  /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
    * KPIBuilder constructor.
    *
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
@@ -53,17 +78,25 @@ class KPIBuilder implements KPIBuilderInterface {
    *   The kpi dataformatter manager.
    * @param \Drupal\kpi_analytics\Plugin\KPIVisualizationManager $kpi_visualization_manager
    *   The kpi visualization manager.
+   * @param \Drupal\layout_builder\SectionStorage\SectionStorageManagerInterface $section_storage_manager
+   *   The section storage manager.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The current route match.
    */
   public function __construct(
     EntityTypeManagerInterface $entity_type_manager,
     KPIDatasourceManager $kpi_datasource_manager,
     KPIDataFormatterManager $kpi_data_formatter_manager,
-    KPIVisualizationManager $kpi_visualization_manager
+    KPIVisualizationManager $kpi_visualization_manager,
+    SectionStorageManagerInterface $section_storage_manager,
+    RouteMatchInterface $route_match
   ) {
     $this->entityTypeManager = $entity_type_manager;
     $this->kpiDatasourceManager = $kpi_datasource_manager;
     $this->kpiDataFormatterManager = $kpi_data_formatter_manager;
     $this->kpiVisualizationManager = $kpi_visualization_manager;
+    $this->sectionStorageManager = $section_storage_manager;
+    $this->routeMatch = $route_match;
   }
 
   /**
@@ -77,6 +110,24 @@ class KPIBuilder implements KPIBuilderInterface {
     if ($block_id) {
       $block = $this->entityTypeManager->getStorage('block')
         ->load($block_id);
+      if (!$block) {
+        if ($node = $this->routeMatch->getParameter('node')) {
+          /** @var \Drupal\layout_builder\SectionStorageInterface $section_storage */
+          $section_storage = $this->getSectionStorageForEntity($node);
+        }
+        else {
+          $section_storage = $this->routeMatch->getParameter('section_storage');
+        }
+        if ($section_storage instanceof SectionStorageInterface) {
+          $sections = $section_storage->getSections();
+          foreach ($sections as $section) {
+            if ($section->getComponent($block_id)) {
+              $block = $section->getComponent($block_id);
+              break;
+            }
+          }
+        }
+      }
     }
     else {
       $block = NULL;
@@ -112,6 +163,36 @@ class KPIBuilder implements KPIBuilderInterface {
       ->render($data);
 
     return $render_array;
+  }
+
+  /**
+   * Gets the section storage for an entity.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity.
+   *
+   * @return \Drupal\layout_builder\SectionStorageInterface|null
+   *   The section storage if found otherwise NULL.
+   */
+  protected function getSectionStorageForEntity(EntityInterface $entity) {
+    // @todo Take into account other view modes in
+    //   https://www.drupal.org/node/3008924.
+    $view_mode = 'full';
+    if ($entity instanceof LayoutEntityDisplayInterface) {
+      $contexts['display'] = EntityContext::fromEntity($entity);
+      $contexts['view_mode'] = new Context(new ContextDefinition('string'), $entity->getMode());
+    }
+    else {
+      $contexts['entity'] = EntityContext::fromEntity($entity);
+      if ($entity instanceof FieldableEntityInterface) {
+        $display = EntityViewDisplay::collectRenderDisplay($entity, $view_mode);
+        if ($display instanceof LayoutEntityDisplayInterface) {
+          $contexts['display'] = EntityContext::fromEntity($display);
+        }
+        $contexts['view_mode'] = new Context(new ContextDefinition('string'), $view_mode);
+      }
+    }
+    return $this->sectionStorageManager->findByContext($contexts, new CacheableMetadata());
   }
 
 }
--- src/Plugin/KPIDatasource/DrupalKPITermDatasource.php
+++ src/Plugin/KPIDatasource/DrupalKPITermDatasource.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\kpi_analytics\Plugin\KPIDatasource;
 
+use Drupal\block\BlockInterface;
 use Drupal\block_content\BlockContentInterface;
 use Drupal\kpi_analytics\Plugin\KPIDatasourceBase;
+use Drupal\layout_builder\SectionComponent;
 
 /**
  * Provides a 'DrupalKPIDatasource' KPI Datasource.
@@ -21,23 +23,35 @@ class DrupalKPITermDatasource extends KPIDatasourceBase {
   public function query(BlockContentInterface $entity, $block) {
     $data = [];
     $args = [];
-    if (
-      $block !== NULL &&
-      $block->getThirdPartySetting('kpi_analytics', 'taxonomy_filter_terms')
-    ) {
-      $query = $entity->field_kpi_query->value;
-      preg_match_all('/:(\w+)/', $query, $placeholders);
-      if (!empty($placeholders[1])) {
-        foreach ($placeholders[1] as $placeholder) {
-          if ($placeholder === 'ids') {
-            $args[':ids[]'] = $block->getThirdPartySetting('kpi_analytics', 'taxonomy_filter_terms');
-          }
+
+    if ($block === NULL) {
+      return $data;
+    }
+    if ($block instanceof BlockInterface) {
+      $block_settings = $block->get('settings');
+    }
+    elseif ($block instanceof SectionComponent) {
+      $block_settings = $block->get('configuration');
+    }
+    else {
+      return $data;
+    }
+    if (empty($block_settings['taxonomy_filter'])) {
+      return $data;
+    }
+
+    $query = $entity->field_kpi_query->value;
+    preg_match_all('/:(\w+)/', $query, $placeholders);
+    if (!empty($placeholders[1])) {
+      foreach ($placeholders[1] as $placeholder) {
+        if ($placeholder === 'term_ids') {
+          $args[':term_ids[]'] = $block_settings['taxonomy_filter'];
         }
       }
-      $results = $this->database->query($query, $args)->fetchAll();
-      foreach ($results as $result) {
-        $data[] = (array) $result;
-      }
+    }
+    $results = $this->database->query($query, $args)->fetchAll();
+    foreach ($results as $result) {
+      $data[] = (array) $result;
     }
 
     return $data;
